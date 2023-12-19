da2d2326a0dac1f78b1ebe0805e3bb7690ab4fea
Jesse Torres <20848952+JesseWebDotCom@users.noreply.github.com>
2023-12-19 13:57:38 -0500

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # plex-border-collection-posters
-Draws borders around all Plex collection posters
+Script which adds border outlines to all Plex collection posters (making them quickly/easlly visually distiniguishable from other posters).
+
+<table>
+<tr style="font-size:2vw"><td>Original</td><td>Modified</td></tr>
+<tr><td><img src=original.png width=150px></td><td><img src=modified.png width=150px></td></tr>
+</table>
+You can then either manually update your collection posters in Plex or use Plex Meta Manager (PMM) to do this for you
+
+## Script Origin
+
+I found myself pausing to distinguish between collections and movies - this is expecially true when the poster for the collection is similar or identical to a movie poster or you just have a lot of movies/collections. Sure I can go to the 'Collections" Plex view to solve this problem, but I still wanted an exsy visual distinction for when I'm in library view, searching, etc.
+
+I use Plex Meta Manager (PMM) and it does not have the ability to draw overlays on collections. I also didn't want to manually draw borders around every collection poster - so I made this script.
+    
+## What if you don't use PMM
+
+Then just ignore any references I mention to it below.  The script will still download and create bordered posters - you will just have to manually update plex with these new posters.
+
+## Requirements
+
+- plex
+- python
+- script dependencies (run `pip install plexapi pillow`)
+- pmm
+
+## Setup
+
+create your desired location to store the original and modified collection posters
+ex. `D:\docker\plex-meta-manager\config\assets\collections\bordered\movies`
+
+edit the script and set your `PLEX_URL` and `PLEX_TOKEN` values
+
+set `assets_for_all` true for your library in your PMM config. ex.
+```yaml
+libraries:
+    Movies:
+        operations:
+        assets_for_all: true
+```
+
+## Usage
+   
+1) run the script, ex.
+```bash
+python .\border_collection_posters.py --action upload --library-name Movies --asset-directory "D:\docker\plex-meta-manager\config\assets\collections\bordered\movies"
+```
+
+In your specified asset directory, downloaded posters will go the "originals" folder and bordered posters will go to the "modifued" folder (these folders will automatically be craeted for you)
+
+2) run PMM operations on your library, ex.
+```bash
+python plex_meta_manager.py --run --ignore-schedules --run-libraries "Movies" --operations-only
+```
+
+## Options
+
+* to only download posters, set action to `download`
+* to only list collections, set action to `list`
+
+### Customizing the borders
+
+By default, the outer border is white with a small black inner border (which helps in case the post is mostly white, thus making the outer border invisible).
+The borders are drawn relative in size to the original poster (2% for outer, 1% for inner). This makes them look all the same size in Plex.
+If you want to change the color or relative percentage size, just add these aguments and change your desired values:
+
+    `--outer-border-percent 2 --inner-border-percent 1 --outer-border-color white --inner-border-color black``
+
+
+## Caching
+To make the script run as quickly as possible, the script will 1) skip any collection poster already downloaded and 2) will not create a new bordered poster and or upload one if already exists.
+
+You can clear these folders as needed manually and then script will repopulate them on next run.
+    
+This does not change how PMM caches and or when PMM decides to use these borded posters.
+
+## Known issues / limitations
+* you may have collections that appear to only have a top and bottom border in plex (but display perfect when manually viewing the poster in the "modified" folder).  This is likely plex cutting off the poster due to it being a non-standard size.
+* pmm does not update your posters. Ex. lets say you run the script to create white thin bordered posters and then run pmm operations.  pmm successfully updates plex and your white thin bordered posters display fine in plex. 
+    but you don't like the color and you delete this scripts cached files (i.e. "originals" and "modified" folders), and re-run pmm operations.  Plex still shows the old posters.
+* posters for collections with non-alphanumeric characters in the name do not get updated. this is likely because PMM and this script replaces those characters with different values.  log a github issue showing the collection name from both PMM and this script's outputs/logs.
+* probably more
+
+## Support
+
+I don't plan on addressing bugs, enhancement request, etc - but log them in Github anyway (I may change my mind)    
---
+++ border_collection_posters.py
@@ -0,0 +1,193 @@
+import argparse
+import os
+from plexapi.server import PlexServer
+from PIL import Image, ImageDraw
+import requests
+from io import BytesIO
+import re
+
+# Constants
+PLEX_URL = "http://127.0.0.1:32400"  # Replace with your actual plex url
+PLEX_TOKEN = "acb123xyzblahreplaceme"  # Replace with your actual plex token
+
+def create_directory(directory_path):
+    os.makedirs(directory_path, exist_ok=True)
+
+def calculate_border_size(width, percentage):
+    return round(width * (percentage / 100))
+
+def sanitize_file_name(file_name):
+    # Remove /
+    file_name = file_name.replace('/', '')
+
+    # Remove :
+    file_name = file_name.replace(':', '')
+
+    # Define a regular expression to match invalid characters including | and /
+    invalid_chars = re.compile(r'[<>"\\|?*\x00-\x1F]')
+
+    # Replace invalid characters with underscores
+    return re.sub(invalid_chars, '_', file_name)
+
+def download_poster(item, title):
+    poster_url = f"{PLEX_URL}{item.thumb}"
+
+    # Download the poster image
+    print(f"{title:<50}{'DOWNLOADING':<15}", end="\r", flush=True)
+    response = requests.get(poster_url, headers={"X-Plex-Token": PLEX_TOKEN})
+    return Image.open(BytesIO(response.content))
+
+def save_original(original_path, original_image):
+    # Save the original poster to the temp folder
+    original_image.save(original_path, format='PNG')
+
+def get_paths(title, asset_directory):
+    original_path = os.path.join(asset_directory, "originals", f"{title}.png")
+    modified_path = os.path.join(asset_directory, "modified", f"{title}.png")
+    return original_path, modified_path
+
+def draw_border(image, border_size, outer_color, inner_color, inner_size):
+    # Draw the outer border
+    draw = ImageDraw.Draw(image)
+    width, height = image.size
+    draw.rectangle([0, 0, width, height], outline=outer_color, width=border_size)
+
+    # Draw the inner border
+    draw.rectangle([border_size, border_size, width - border_size, height - border_size], outline=inner_color, width=inner_size)
+
+def process_upload(original_image, modified_path, title, outer_border_percent, inner_border_percent, outer_border_color, inner_border_color):
+    global uploaded_count, error_count  # Declare global variables
+
+    # Calculate the border sizes based on percentages
+    width, height = original_image.size
+    outer_border_size = calculate_border_size(width, outer_border_percent)
+    inner_border_size = calculate_border_size(width, inner_border_percent)
+
+    # Create a new image with the original size
+    bordered_image = Image.new("RGB", (width, height), "white")
+
+    # Paste the original image onto the new image
+    bordered_image.paste(original_image, (0, 0))
+
+    # Draw borders on the new image
+    draw_border(bordered_image, outer_border_size, outer_border_color, inner_border_color, inner_border_size)
+
+    # Save the modified poster to the temp folder
+    bordered_image.save(modified_path, format='PNG')
+    print(f"{title:<50}{'UPLOADED':<15}", end="\r", flush=True)
+    uploaded_count += 1
+
+def process_collection(item, action, asset_directory, outer_border_percent, inner_border_percent, outer_border_color, inner_border_color):
+    global downloaded_count, modified_count, error_count, skipped_count  # Declare global variables
+
+    title = sanitize_file_name(item.title)
+
+    if action =="list":
+        print(f"\n{title:<50}{'SKIPPED':<15}Action is list", end="\r", flush=True)
+        skipped_count += 1
+        return
+
+    print(f"\n{title:<50}{'PROCESSING':<15}", end="\r", flush=True)
+
+    # Get the collection poster URL
+    if item.thumb is None:
+        print(f"{title:<50}{'SKIPPED':<15}No poster found", end="\r", flush=True)
+        skipped_count += 1
+        return
+
+    # Calculate the paths for original and modified images
+    create_directory(os.path.join(asset_directory, "originals"))
+    create_directory(os.path.join(asset_directory, "modified"))
+    original_path, modified_path = get_paths(title, asset_directory)
+
+    # Check if the modified file already exists
+    if os.path.exists(modified_path):
+        print(f"{title:<50}{'SKIPPED':<15}Modified already exists", end="\r", flush=True)
+        skipped_count += 1
+        return
+
+    # Check if the original already exists
+    if os.path.exists(original_path):
+        original_image = Image.open(original_path)
+    else:
+        try:
+            print(f"{title:<50}{'DOWNLOADING':<15}", end="\r", flush=True)
+            original_image = download_poster(item, title)
+            save_original(original_path, original_image)
+            downloaded_count += 1
+        except Exception as e:
+            print(f"{title:<50}{'ERRORED':<15}{'Downloading: ' + str(e).title()}", end="\r", flush=True)
+            error_count += 1
+            return
+
+    # Draw borders around the original image
+    if action == "upload":
+        try:
+            print(f"{title:<50}{'UPLOADING':<15}", end="\r", flush=True)
+            process_upload(original_image, modified_path, title, outer_border_percent, inner_border_percent, outer_border_color, inner_border_color)
+            modified_count += 1
+        except Exception as e:
+            print(f"{title:<50}{'ERRORED':<15}{'Uploading: ' + str(e).title()}", end="\r", flush=True)
+            error_count += 1
+       
+# Global counters
+downloaded_count = 0
+modified_count = 0
+uploaded_count = 0
+error_count = 0
+skipped_count = 0
+
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Download and modify Plex collection posters.")
+parser.add_argument("--action", choices=["download", "upload", "list"], default="download", nargs="?", help="Action to specify the actions to be taken.")
+parser.add_argument("--library-name", required=True, help="Name of the Plex library.")
+parser.add_argument("--asset-directory", required=True, help="Path to the asset directory.")
+parser.add_argument("--outer-border-percent", type=float, default=2, help="Percentage of outer border size relative to the image width.")
+parser.add_argument("--inner-border-percent", type=float, default=1, help="Percentage of inner border size relative to the image width.")
+parser.add_argument("--outer-border-color", default="white", help="Color of the outer border.")
+parser.add_argument("--inner-border-color", default="black", help="Color of the inner border.")
+args = parser.parse_args()
+
+# Print script arguments
+print("Script Arguments:")
+print(f"  Action: {args.action}")
+print(f"  Library Name: {args.library_name}")
+print(f"  Asset Directory: {args.asset_directory}")
+print(f"  Outer Border Percent: {args.outer_border_percent}")
+print(f"  Inner Border Percent: {args.inner_border_percent}")
+print(f"  Outer Border Color: {args.outer_border_color}")
+print(f"  Inner Border Color: {args.inner_border_color}")
+print("")
+
+# Normalize the asset directory path
+args.asset_directory = os.path.normpath(args.asset_directory)
+
+# Create asset directory as needed
+create_directory(args.asset_directory)
+
+# Connect to Plex server
+plex = PlexServer(PLEX_URL, PLEX_TOKEN)
+
+# Get library and collection items
+lib = next((s for s in plex.library.sections() if s.title == args.library_name), None)
+items = lib.search(libtype="collection")
+
+# Process each collection item
+print("Processing collections:")
+print('_' * 100)
+
+for collection in items:
+    process_collection(collection, args.action, args.asset_directory, args.outer_border_percent, args.inner_border_percent, args.outer_border_color, args.inner_border_color)
+
+# Print total counts
+print("")
+print('_' * 100)
+print("\nTotal Counts:")
+print(f"  Collections: {len(items)}")
+print(f"    Skipped: {skipped_count}")
+print(f"    Modified: {modified_count}")
+print(f"      Downloaded: {downloaded_count}")
+print(f"      Uploaded: {uploaded_count}")
+print(f"    Errors: {error_count}")
+print("")
+print("Done")
